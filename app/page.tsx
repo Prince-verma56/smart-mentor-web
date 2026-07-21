392247dd71849e6d89d9bc07d0db0c9c6d39f07a
Prince-verma56 <devincreator123@gmail.com>
2026-07-21 15:17:29 +0530

    latest update in c4

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation  
           </a>
         </div>
       </main>
